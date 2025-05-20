e332b48b739332631046903698ccfe8a9f8335a6
Perkin <lauljfa05@gmail.com>
2025-05-19 23:39:40 -0300

    No anda el localcoso waaa

--- src/components/SeccionAlquiler.jsx
+++ src/components/SeccionAlquiler.jsx
@@ -1,13 +1,14 @@
+import {imgUno} from "../assets/alquiler-uno.jpg"
+
 const SecccionAlquiler = () => {
     return (
         <section className="w-screen h-screen overflow-hidden bg-zinc common-padding">
             <div className="flex justify-between">
-                <div className="flex-column">
-                    <p>HOLA MUNDO</p>
-                    <p>Eleva tu capacidad de captura</p>
+                <div>
+                    <img src={imgUno} alt="Equipos para alquilar" />
                 </div>
                 <div>
-                    <p>IMAGEn</p>
+                    <p> Lista de las cosas para alquilar </p>
                 </div>
             </div>
         </section>
