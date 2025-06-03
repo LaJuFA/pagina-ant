1355d1f50f362f5345725fbc1ca2ead02970ed4d
Perkin <lauljfa05@gmail.com>
2025-06-03 11:38:15 -0300

    Cositas de alquiler

--- src/components/SeccionAlquiler.jsx
+++ src/components/SeccionAlquiler.jsx
@@ -2,13 +2,19 @@ import imgUno from "../assets/alquiler-uno.jpg"
 
 const SecccionAlquiler = () => {
     return (
-        <section className="w-screen h-screen overflow-hidden bg-zinc common-padding">
-            <div className="flex justify-between">
-                <div>
-                    <img src={imgUno} alt="Equipos para alquilar" />
+        <section className="w-screen h-screen overflow-hidden bg-zinc pt-24 pr-16 pl-16">
+            <div className="flex justify-between gap-4">
+                <div className="w-3/5">
+                    <img src={imgUno} alt="Equipos para alquilar" className="rounded-xl" />
                 </div>
-                <div>
-                    <p> Lista de las cosas para alquilar </p>
+                <div className="flex flex-col w-2/5 text-zinc font-helvetica text-2xl font-bold gap-4">
+                    <div className="bg-[#f7f500] h-3/4 rounded-xl p-4">
+                        <p>- Sobre el precio -</p>
+                        <p>Para informacion exacta del precio de cada equipo contactarse por el numero de telefono</p>
+                    </div>
+                    <div className="bg-[#f7f500] h-1/4 rounded-xl p-4">
+                        <p>- Lista de equipos a la venta -</p>
+                    </div>
                 </div>
             </div>
         </section>
