d894957489d2626342e9d4b131b4bf370073fb89
Sebastian Flores <lihuensfa@gmail.com>
2025-06-11 20:43:02 -0300

    cambie la disposición de los multiples proyectos

--- src/components/SeccionProyectos.jsx
+++ src/components/SeccionProyectos.jsx
@@ -4,22 +4,22 @@ const SeccionProyectos = () =>{
     <section className="h-full w-screen overflow-hidden bg-zinc common-padding">
       <div className="w-[100%] h-[100%] flex-col justify-center">
         <div className="h-[100%] flex justify-center gap-10 pb-4">
-          <div className="h-[14rem] w-[8rem] border-2 border-white rounded"></div>
-          <div className="h-[14rem] w-[8rem] border-2 border-white rounded"></div>
-          <div className="h-[14rem] w-[8rem] border-2 border-white rounded"></div>
-          <div className="h-[14rem] w-[8rem] border-2 border-white rounded"></div>
-          <div className="h-[14rem] w-[8rem] border-2 border-white rounded"></div>
-          <div className="h-[14rem] w-[8rem] border-2 border-white rounded"></div>
-
+          <div className="h-[8rem] w-[14rem] border-2 border-white rounded"></div>
+          <div className="h-[8rem] w-[14rem] border-2 border-white rounded"></div>
+          <div className="h-[8rem] w-[14rem] border-2 border-white rounded"></div>
+          <div className="h-[8rem] w-[14rem] border-2 border-white rounded"></div>
+        </div>
+        <div className="h-[100%] flex justify-center gap-10 pb-4">
+          <div className="h-[8rem] w-[14rem] border-2 border-white rounded"></div>
+          <div className="h-[8rem] w-[14rem] border-2 border-white rounded"></div>
+          <div className="h-[8rem] w-[14rem] border-2 border-white rounded"></div>
+          <div className="h-[8rem] w-[14rem] border-2 border-white rounded"></div>
         </div>
         <div className="h-[100%] flex justify-center gap-10">
-          <div className="h-[14rem] w-[8rem] border-2 border-white rounded"></div>
-          <div className="h-[14rem] w-[8rem] border-2 border-white rounded"></div>
-          <div className="h-[14rem] w-[8rem] border-2 border-white rounded"></div>
-          <div className="h-[14rem] w-[8rem] border-2 border-white rounded"></div>
-          <div className="h-[14rem] w-[8rem] border-2 border-white rounded"></div>
-          <div className="h-[14rem] w-[8rem] border-2 border-white rounded"></div>
-
+          <div className="h-[8rem] w-[14rem] border-2 border-white rounded"></div>
+          <div className="h-[8rem] w-[14rem] border-2 border-white rounded"></div>
+          <div className="h-[8rem] w-[14rem] border-2 border-white rounded"></div>
+          <div className="h-[8rem] w-[14rem] border-2 border-white rounded"></div>
         </div>
       </div>
     </section>
