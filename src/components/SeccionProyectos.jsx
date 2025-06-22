19137b93fef964574bed70d73e6b50eb0ba2fb61
Sebastian Flores <lihuensfa@gmail.com>
2025-06-22 19:25:34 -0300

    añadi animación a las tarjetas

--- src/components/SeccionProyectos.jsx
+++ src/components/SeccionProyectos.jsx
@@ -1,61 +1,52 @@
+import { Link } from "react-router-dom";
+import imgPH from "../assets/nosotros-uno.jpg"
+
 const SeccionProyectos = () =>{
 
   return (
     <section className="h-full w-screen overflow-hidden bg-zinc common-padding">
       <div className="w-[100%] h-[100%] flex-col justify-center text-white">
         <div className="h-[100%] flex justify-center gap-10 pb-4">
+          <div className='card__container'>
+            <article className="card__article">
+              <img src={imgPH} alt="Imagen representando un frame de un proyecto" className="card__img"/>
+              <div className="card__data">
+                <span className="card__desc">Producción: 2020</span>
+                <h2 className="card__title">Conociendo Ucrania</h2>
+                <Link></Link>
+              </div>
+            </article>
+            <article className="card__article">
+              <img src={imgPH} alt="Imagen representando un frame de un proyecto" className="card__img"/>
+              <div className="card__data">
+                <span className="card__desc">Producción: 2020</span>
+                <h2 className="card__title">Conociendo Ucrania</h2>
+                <Link></Link>
+              </div>
+            </article>
+            <article className="card__article">
+              <img src={imgPH} alt="Imagen representando un frame de un proyecto" className="card__img"/>
+              <div className="card__data">
+                <span className="card__desc">Producción: 2020</span>
+                <h2 className="card__title">Conociendo Ucrania</h2>
+                <Link></Link>
+              </div>
+            </article>
+            <article className="card__article">
+              <img src={imgPH} alt="Imagen representando un frame de un proyecto" className="card__img"/>
+              <div className="card__data">
+                <span className="card__desc">Producción: 2020</span>
+                <h2 className="card__title">Conociendo Ucrania</h2>
+                <Link></Link>
+              </div>
+            </article>
+          </div>
+          {/* 
           <div className="h-[10rem] w-[20rem] border-2 border-white p-4 rounded flex flex-col justify-end hover:h-[12rem] hover:w-[22rem]">
             <h2 className="font-bold text-2xl">Conociendo Ucrania</h2>
             <p>Producción: 2020</p>
           </div>
-          <div className="h-[10rem] w-[20rem] border-2 border-white p-4 rounded flex flex-col justify-end hover:h-[12rem] hover:w-[22rem]">
-            <h2 className="font-bold text-2xl">Conociendo Ucrania</h2>
-            <p>Producción: 2020</p>
-          </div>
-          <div className="h-[10rem] w-[20rem] border-2 border-white p-4 rounded flex flex-col justify-end hover:h-[12rem] hover:w-[22rem]">
-            <h2 className="font-bold text-2xl">Conociendo Ucrania</h2>
-            <p>Producción: 2020</p>
-          </div>
-          <div className="h-[10rem] w-[20rem] border-2 border-white p-4 rounded flex flex-col justify-end hover:h-[12rem] hover:w-[22rem]">
-            <h2 className="font-bold text-2xl">Conociendo Ucrania</h2>
-            <p>Producción: 2020</p>
-          </div>          
-        </div>
-        <div className="h-[100%] flex justify-center gap-10 pb-4">
-          <div className="h-[10rem] w-[20rem] border-2 border-white p-4 rounded flex flex-col justify-end hover:h-[12rem] hover:w-[22rem]">
-            <h2 className="font-bold text-2xl">Conociendo Ucrania</h2>
-            <p>Producción: 2020</p>
-          </div>
-          <div className="h-[10rem] w-[20rem] border-2 border-white p-4 rounded flex flex-col justify-end hover:h-[12rem] hover:w-[22rem]">
-            <h2 className="font-bold text-2xl">Conociendo Ucrania</h2>
-            <p>Producción: 2020</p>
-          </div>
-          <div className="h-[10rem] w-[20rem] border-2 border-white p-4 rounded flex flex-col justify-end hover:h-[12rem] hover:w-[22rem]">
-            <h2 className="font-bold text-2xl">Conociendo Ucrania</h2>
-            <p>Producción: 2020</p>
-          </div>
-          <div className="h-[10rem] w-[20rem] border-2 border-white p-4 rounded flex flex-col justify-end hover:h-[12rem] hover:w-[22rem]">
-            <h2 className="font-bold text-2xl">Conociendo Ucrania</h2>
-            <p>Producción: 2020</p>
-          </div>
-        </div>
-        <div className="h-[100%] flex justify-center gap-10">
-          <div className="h-[10rem] w-[20rem] border-2 border-white p-4 rounded flex flex-col justify-end hover:h-[12rem] hover:w-[22rem]">
-            <h2 className="font-bold text-2xl">Conociendo Ucrania</h2>
-            <p>Producción: 2020</p>
-          </div>
-          <div className="h-[10rem] w-[20rem] border-2 border-white p-4 rounded flex flex-col justify-end hover:h-[12rem] hover:w-[22rem]">
-            <h2 className="font-bold text-2xl">Conociendo Ucrania</h2>
-            <p>Producción: 2020</p>
-          </div>
-          <div className="h-[10rem] w-[20rem] border-2 border-white p-4 rounded flex flex-col justify-end hover:h-[12rem] hover:w-[22rem]">
-            <h2 className="font-bold text-2xl">Conociendo Ucrania</h2>
-            <p>Producción: 2020</p>
-          </div>
-          <div className="h-[10rem] w-[20rem] border-2 border-white p-4 rounded flex flex-col justify-end hover:h-[12rem] hover:w-[22rem]">
-            <h2 className="font-bold text-2xl">Conociendo Ucrania</h2>
-            <p>Producción: 2020</p>
-          </div>  
+          */}
         </div>
       </div>
     </section>
